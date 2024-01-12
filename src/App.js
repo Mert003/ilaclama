f2cbff8a8f2583169b38958625cf902a8c2acd99
Osman Mert Sevil <osman.sevil003@gmail.com>
2024-01-12 15:33:52 +0300

    second21

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Router,Routes , Route } from 'react-router-dom';
+import { Routes , Route } from 'react-router-dom';
 import NavigationBar from './components/navbar/navbar';
 import Home from './pages/home';
 
@@ -7,12 +7,10 @@ function App() {
   return (
     <div className="App">
       <NavigationBar />
-      <Router>
           <Routes  forceRefresh={true}>
                <Route path="/" element={<Home /> } />
               
           </Routes>
-          </Router>
     </div>
   );
 }
