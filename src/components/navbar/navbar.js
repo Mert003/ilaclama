6c94e94f4485d00d5e12506c85d61400ee91e39e
Osman Mert Sevil <osman.sevil003@gmail.com>
2024-01-13 13:35:16 +0300

    duzenlenme21

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 /* import 'bootstrap/dist/css/bootstrap.min.css'; */
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import { PiNumberEight } from "react-icons/pi";
 
 import { Link } from 'react-router-dom';
 import "./navbar.css";
